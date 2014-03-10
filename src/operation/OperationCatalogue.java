55d04f49a41b749e4e704000630a74dededd25c4
Ali <a70babat@gmail.com>
2014-03-10 15:17:36 +0330

    font added

---
+++ src/operation/OperationCatalogue.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package operation;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author ali
+ */
+public class OperationCatalogue {
+    
+    private static OperationCatalogue instance = null;
+    
+    private final Vector<Operation> operations = new Vector<Operation>();
+    
+    private OperationCatalogue(){
+        /*mocking*/
+        int t = 4;
+        while(t-- > 0){
+            Operation operation = new Operation();
+            operation.setName("باقر");
+            operations.add(operation);
+            operation = new Operation();
+            operation.setName("تبریزی");
+            operations.add(operation);
+            operation = new Operation();
+            operation.setName("کوهی");
+            operations.add(operation);
+        }
+    }
+    
+    public static OperationCatalogue getInstace(){
+        if(instance == null)
+            instance = new OperationCatalogue();
+        return instance;
+    }
+    
+    public void addOperation(Operation operation){
+        operations.add(operation);
+    }
+    
+    public Boolean remove(Operation operation){
+        return operations.remove(operation);
+    }
+    
+    public Vector<Operation> getOperations(){
+        return operations;
+    }
+}
