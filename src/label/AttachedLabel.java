93df089165ae073d06b79c0bcdb138e6003b7461
Ali <a70babat@gmail.com>
2014-03-16 15:59:02 +0330

    operation and attachedlabel completed

---
+++ src/label/AttachedLabel.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package label;
+
+import asset.Asset;
+import exceptions.DuplicatedValueException;
+import java.util.Vector;
+
+/**
+ *
+ * @author ali
+ */
+public class AttachedLabel {
+    private Label label;
+    private Asset asset;
+    private Vector<String> values;
+
+    public AttachedLabel() {
+        values = new Vector<>();
+    }
+    
+    public boolean addValue(String value) throws DuplicatedValueException {
+        if(values.indexOf(value) != -1)
+            throw new DuplicatedValueException();
+        if(label != null && label.isValidValue(value)){
+            values.add(value);
+            return true;
+        }
+        return false;
+    }
+    
+    public Object[] getValues(){
+        return values.toArray();
+    }
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public void setLabel(Label label) {
+        values = new Vector<>();
+        this.label = label;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
+
+    @Override
+    public String toString() {
+        if(label != null)
+            return label.getName();
+        else{
+            System.err.println(asset);
+            System.err.println(label);
+            System.err.println(values);
+            return "";
+        }
+            
+    }
+}
